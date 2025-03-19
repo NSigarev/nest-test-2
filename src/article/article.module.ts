6107cd4b776d06b78b01d5876abf352a2f13b7e2
kinda_funnyz <nikita_sigarev@mail.ru>
2025-03-19 13:34:38 +0700

    0.9.0 cache

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ArticleController } from "./article.controller";
 import { ArticleService } from "./article.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Article } from "./entity/article.entity";
+import { Article } from './entity/article.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Article])],
+  imports: [
+    TypeOrmModule.forFeature([Article]),
+    CacheModule.register()],
   controllers: [ArticleController],
   providers: [ArticleService]
 })
